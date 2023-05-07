6deb5facd04b1585456b4434163f693fda5ad175
Ariana Slaviček <aslavicek21@foi.hr>
2023-05-07 20:03:43 +0200

    Implementiran prikaz liste studenata

--- EvaluationManager/FrmStudents.cs
+++ EvaluationManager/FrmStudents.cs
@@ -9,9 +9,20 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EvaluationManager {
+
     public partial class FrmStudents : Form {
         public FrmStudents() {
             InitializeComponent();
+        }
+
+        private void ShowStudents() {
+            StudentRepository repository = new StudentRepository();
+            List<Student> students = repository.GetStudents();
+            dgvStudents.DataSource = students;
+        }
+
+        private void FrmStudents_Load(object sender, EventArgs e) {
+            ShowStudents();
         }
     }
 }
---
+++ EvaluationManager/StudentRepository.cs
@@ -0,0 +1,52 @@
+﻿using DBLayer;
+using EvaluationManager;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvaluationManager {
+
+    public class StudentRepository {
+        public Student CreateObject(SqlDataReader reader) {
+            Student student = new Student();
+            student.Id = Convert.ToInt32(reader["Id"].ToString());
+            student.FirstName = reader["FirstName"].ToString();
+            student.LastName = reader["LastName"].ToString();
+            int.TryParse(reader["Grade"].ToString(), out int grade);
+            student.Grade = grade;
+
+            return student;
+        }
+        public Student GetStudent(int id) {
+            Student student = null;
+            DB.OpenConnection();
+            SqlDataReader reader = DB.GetDataReader($"SELECT * FROM Students WHERE Id = {id}");
+            if (reader.HasRows) {
+                reader.Read();
+                student = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return student;
+        }
+        public List<Student> GetStudents() {
+            List<Student> students = new List<Student>();
+            DB.OpenConnection();
+            SqlDataReader reader = DB.GetDataReader($"SELECT * FROM Students");
+            while (reader.Read()) { 
+                if (reader.HasRows) {
+                    students.Add(CreateObject(reader));
+                }
+            }
+            reader.Close();
+            DB.CloseConnection();
+            return students;
+        }
+
+        
+    }
+}
